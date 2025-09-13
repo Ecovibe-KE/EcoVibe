74ad962e74c611cc275bae66b54715cb518372b7
Ian Otsembo <okumu.otsembo@gmail.com>
2025-09-13 16:32:06 +0300

    test coderabbit

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "EcoVibe Completed Website"
+    return "EcoVibe Completed Website!"
 
 if __name__ == "__main__":
     app.run(debug=True)
