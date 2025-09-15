34631c1016670c5d1dd282529ccfb282650a544c
CAROLINE ININGA MAJUTE <carolineininga@gmail.com>
2025-09-15 18:24:45 +0300

    updated client and server folder structure (#40)

---
+++ client/src/components/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import '../css/App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <p>Welcome to Ecovibe</p>
+    <p>Something good is coming soon!</p>
+    </>
+  )
+}
+
+export default App
