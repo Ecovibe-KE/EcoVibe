fb23afd4dbcf7c9f3fb679794b88dae791c4d9e1
Ian Otsembo <okumu.otsembo@gmail.com>
2025-09-16 21:30:48 +0300

    chore: replace the client working directory on CI

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,10 +8,10 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './jest.setup.js',
-    reporter: ['default', 'html'],
     provider: 'v8',
     coverage: {
       reporter: ['text', 'json-summary', 'html'],
+      reportOnFailure: true,
     },
   },
 })
