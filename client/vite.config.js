42125eb649656410d644e972b7bc005dc7c1ad35
Ian Otsembo <okumu.otsembo@gmail.com>
2025-09-16 21:46:17 +0300

    task: add write requests to PR for vitest on CI

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     setupFiles: './jest.setup.js',
     provider: 'v8',
     coverage: {
-      reporter: ['text', 'json-summary', 'html'],
+      reporter: ['text', 'json', 'html'],
       reportOnFailure: true,
     },
   },
