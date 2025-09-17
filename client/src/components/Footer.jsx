75e5a451524144d12f2bba7c8661d1230cfcb60c
Felistus Kimanthi <kimanthi.felistus@gmail.com>
2025-09-17 14:50:10 +0300

    linkedin url update

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -88,10 +88,10 @@ const Footer = ({ pageType }) => {
                   <FaInstagram size={20} />
                 </a>
                 <a
-                  href="https://www.linkedin.com/company/ecovibe-ke"
+                  href="https://www.linkedin.com/company/ecovibe-kenya/"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="text-light"
+                  className="text-light me-3"
                 >
                   <FaLinkedin size={20} />
                 </a>
