bdbb9b5c84b419731f7a6f94beb1badbea3d2b11
wabwirevalentine <levaksmaita@gmail.com>
2025-09-23 05:19:35 +0300

    url_prefix for easy reusability

--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -1,7 +1,7 @@
 from .contact import contact_bp
 from .user import user_bp
 
-
+API = "/api"
 def register_routes(app):
-    app.register_blueprint(user_bp, url_prefix="/api")
-    app.register_blueprint(contact_bp, url_prefix="/api")
+    app.register_blueprint(user_bp, url_prefix=API)
+    app.register_blueprint(contact_bp, url_prefix=API)
