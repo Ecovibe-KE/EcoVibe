b307032f8ea75259d0ab6e804bdf6ea3ccea1c8e
wabwirevalentine <levaksmaita@gmail.com>
2025-09-23 20:21:36 +0300

    remove __init__.py on root also reverted usermodel

--- client/src/components/admin/UserManagement.jsx
+++ client/src/components/admin/UserManagement.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const UserManagement = () => {
-  return <div>User Management</div>;
+  return <div>UserManagement</div>;
 };
 
 export default UserManagement;
