b58daee420d5d26bd02b8b88e0ca8dcc080a501e
wabwirevalentine <41318121+wabwirevalentine@users.noreply.github.com>
2025-09-23 02:22:43 +0000

    [MegaLinter] Apply linters fixes

--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -2,6 +2,8 @@ from .contact import contact_bp
 from .user import user_bp
 
 API = "/api"
+
+
 def register_routes(app):
     app.register_blueprint(user_bp, url_prefix=API)
     app.register_blueprint(contact_bp, url_prefix=API)
