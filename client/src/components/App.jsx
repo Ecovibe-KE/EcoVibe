bf4209d856cf999b7cfa69af7007dda23058d1a8
kali <kinyuaj8234@gmail.com>
2025-09-24 07:24:41 -0400

    feat:running user registration test in locally

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,4 @@
-import Homepage from './Homepage';
+import Homepage from "./Homepage";
 import { useEffect } from "react";
 import { useAnalytics } from "../hooks/useAnalytics";
 import NavBar from "./Navbar.jsx";
@@ -6,6 +6,7 @@ import Playground from "./Playground.jsx";
 import Contact from "./Contact.jsx";
 import { ToastContainer } from "react-toastify";
 import { Routes, Route } from "react-router-dom";
+import SignUpForm from "./Signup.jsx";
 
 function App() {
   const { logEvent } = useAnalytics();
@@ -16,30 +17,39 @@ function App() {
       firebase_screen_class: "App",
     });
   }, [logEvent]);
-    return (
-        <>
-            <NavBar/>
-            <Routes>
-                <Route path="/playground" element={<Playground/>}/>
-                <Route path="/contact" element={<Contact/>}/>
-                <Route path="/" element={<Homepage/>  }/>
-
-
-            </Routes>
-            {/*Reusable toast*/}
-            <ToastContainer
-                position="top-right"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-            />
-        </>
-    );
-
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/playground" element={<Playground />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/signup" element={<SignUpForm />} />
+        <Route
+          path="/login"
+          element={
+            <>
+              <div className="justify-content-center align-content-center">
+                <p className="">Welcome to ecovibe</p>
+                <p className="">Something good is coming</p>
+              </div>
+            </>
+          }
+        ></Route>
+      </Routes>
+      {/*Reusable toast*/}
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </>
+  );
 }
 export default App;
