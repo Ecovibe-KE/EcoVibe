c1fd15b1fc697f8824ab65f9961e458d8eec4e9d
kali <kinyuaj8234@gmail.com>
2025-11-08 15:11:02 +0300

    added a section on the home page and made minor changes to the about us page.

--- client/src/components/AboutUsPartition.jsx
+++ client/src/components/AboutUsPartition.jsx
@@ -3,6 +3,7 @@ import "../css/aboutus.css";
 
 function AboutUsPartition({
   title,
+  subheading,
   contentArray,
   extraSetting,
   padding,
@@ -13,6 +14,8 @@ function AboutUsPartition({
     <>
       <article className="container mb-5">
         <h2 className="mb-5 fw-semibold display-6 about-underline">{title}</h2>
+        <h3 className="text-muted mx-auto fw-bold fw-bold lead mb-5"
+            style={{ maxWidth: "800px" }} >{subheading}</h3>
         <div
           className={`row row-cols-1 row-cols-md-2 ${extraSetting} g-5 justify-content-center`}
         >
